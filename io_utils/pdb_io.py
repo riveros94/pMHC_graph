6da3217ce7bd1326a4d6708106d3d3c12731b7ff
elementare <eyescoderbr@gmail.com>
2025-06-11 13:43:05 -0300

    Updating repo

--- io_utils/pdb_io.py
+++ io_utils/pdb_io.py
@@ -1,12 +1,10 @@
 import os
 
-
 def list_pdb_files(pdb_dir):
     pdb_files = [f for f in os.listdir(pdb_dir) if f.endswith('.pdb')]
     
     if not pdb_files:
-        print("No PDB files found in the directory.")
-        return []
+        raise Exception(f"No PDB files found in the directory: {pdb_dir}")
     
     print("Select PDB files to associate:")
     print("1 - All")
@@ -16,30 +14,31 @@ def list_pdb_files(pdb_dir):
     
     return pdb_files
 
-
 def get_user_selection(pdb_files, pdb_dir):
     selection = input("\nEnter the numbers of the proteins to associate (comma-separated) or '1' to select all: ")
     
-    try:
-        reference_graph_indice = int(input("Enter the number of your reference graph or '1' to let system choose: "))
-        selected_numbers = [int(num.strip()) for num in selection.split(",")]
+    # try:
+        # reference_graph_indice = int(input("Enter the number of your reference graph or '1' to let system choose: "))
+        # selected_numbers = [int(num.strip()) for num in selection.split(",")]
         
-        if reference_graph_indice not in selected_numbers and reference_graph_indice != 1:
-            raise ValueError("The reference graph must be one of the selected proteins.")
-    except ValueError as e:
-        print("Invalid input for reference graph. Please enter a valid number.")
-        return get_user_selection(pdb_files, pdb_dir)
+        # if reference_graph_indice not in selected_numbers and reference_graph_indice != 1:
+        #     raise ValueError("The reference graph must be one of the selected proteins.")
+    # except ValueError as e:
+    #     print("Invalid input for reference graph. Please enter a valid number.")
+    #     return get_user_selection(pdb_files, pdb_dir)
     
-    if reference_graph_indice == 1:
-        reference_graph = None
-    else:
-        try:
-            reference_graph = pdb_files[reference_graph_indice - 2]
-            reference_graph = os.path.join(pdb_dir, reference_graph)
-        except IndexError:
-            print("Reference graph number out of range.")
-            return get_user_selection(pdb_files, pdb_dir)
+    # if reference_graph_indice == 1:
+    #     reference_graph = None
+    # else:
+    #     try:
+    #         reference_graph = pdb_files[reference_graph_indice - 2]
+    #         reference_graph = os.path.join(pdb_dir, reference_graph)
+    #     except IndexError:
+    #         print("Reference graph number out of range.")
+    #         return get_user_selection(pdb_files, pdb_dir)
     
+    selected_numbers = [int(num.strip()) for num in selection.split(",")]
+
     if 1 in selected_numbers:
         selected_files = [[os.path.join(pdb_dir, pdb), pdb] for pdb in pdb_files]
     else:
@@ -50,4 +49,4 @@ def get_user_selection(pdb_files, pdb_dir):
             except IndexError:
                 print(f"Number {i} is out of range. Skipping.")
     
-    return selected_files, reference_graph
+    return selected_files #, reference_graph
